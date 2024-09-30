603d2cf8f2bb5cf08327c3d34088a0521c86d5de
WintrAx <WintrAx@yandex.ru>
2024-09-30 14:34:42 +0300

    function.hpp

---
+++ function.hpp
@@ -0,0 +1,13 @@
+//
+//  function.hpp
+//  students
+//
+//  Created by Алексей Маслов on 27.09.2024.
+//
+// объявляем функции и переменные 
+#ifndef function_hpp
+#define function_hpp
+
+#include <stdio.h>
+
+#endif /* function_hpp */
