a6fff90385afe97ede1d44de203f6e7c2e1d9f60
WintrAx <75830783+Win1rAx@users.noreply.github.com>
2024-09-30 14:06:57 +0300

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,9 @@
+//
+//  function.cpp
+//  students
+//
+//  Created by Алексей Маслов on 27.09.2024.
+//
+// Создаем функции и переменные
+#include "function.hpp"
+#include <iostream>
---
+++ main.cpp
@@ -0,0 +1,138 @@
+//
+//  main.cpp
+//  students
+//
+//  Created by Алексей Маслов on 27.09.2024.
+//
+
+#include <iostream>
+#include "function.hpp"
+#include <fstream>
+using namespace std;
+class Password
+{
+public:
+
+    void Entrance()
+    {
+        string login = "logins.txt";
+        string password = "passwords.txt";
+
+        fstream log;
+        fstream pass;
+
+        log.open(login, fstream::in | fstream::out | fstream::app);
+        pass.open(password, fstream::in | fstream::out | fstream::app);
+
+        if (!log.is_open() && !pass.is_open())
+        {
+            cout << "Ошибка!" << endl;
+        }
+        else
+        {
+            string log_cheker;
+            string pass_cheker;
+
+            while (!log.eof()) //Пока не закончатся символы, идем по циклу.
+            {
+                log_cheker = "";
+                log >> log_cheker; //Передаем данные с файла в переменную.
+            }
+            while (!pass.eof()) //Пока не закончатся символы, идем по циклу.
+            {
+                pass_cheker = "";
+                pass >> pass_cheker; //Передаем данные с файла в переменную.
+            }
+
+            string login_correct;
+            string pass_correct;
+
+            cout << "Введите свой логин: "; cin >> login_correct;
+            cout << endl;
+            cout << "Введите свой пароль: "; cin >> pass_correct;
+
+            if (login_correct != log_cheker && pass_correct != pass_cheker)
+            {
+                cout << "Пароль или логин введен не правильно!" << endl;
+            }
+            else
+            {
+                cout << "Данные введены верно!" << endl;
+            }
+            
+        }
+    }
+    void Sign_up()
+    {
+        string login = "logins.txt";
+        string password = "passwords.txt";
+
+        fstream log;
+        fstream pass;
+
+        log.open(login, fstream::in | fstream::out | fstream::app);
+        pass.open(password, fstream::in | fstream::out | fstream::app);
+
+        if (!log.is_open() && !pass.is_open())
+        {
+            cout << "Ошибка!" << endl;
+        }
+        else
+        {
+            string log_cheker;
+            string pass_cheker;
+            cout << "Введите свой логин: "; cin >> log_cheker;
+            cout << endl;
+            cout << "Введите свой пароль: "; cin >> pass_cheker;
+
+            log << log_cheker;
+            pass << pass_cheker;
+
+            cout << "Регистрация прошла успешно!" << endl;
+
+            
+
+        }
+    }
+
+};
+class menu
+{
+public:
+
+    int RunMenu()
+    {
+        int punkt;
+        cout << "Добро пожаловать!" << endl;
+        cout << "1.Вход" << endl;
+        cout << "2.Регистрация" << endl;
+        cin >> punkt;
+
+        if (punkt != 1 && punkt != 2)
+        {
+            cout << "Выбран неверный пункт меню!" << endl;
+        }
+        if (punkt == 1)
+        {
+            
+            Password pass;
+            pass.Entrance();
+        }
+        if (punkt == 2)
+        {
+            
+            Password pass;
+            pass.Sign_up();
+        }
+        return 0;
+    }
+};
+int main()
+{
+
+    menu Menu;
+
+    Menu.RunMenu();
+
+    return 0;
+}
